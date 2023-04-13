1d0699d56d0dc3555d52b31031e7622470e160e4
evgeny.zuev <evgeny.zuev@tmhsmart.ru>
2023-04-13 17:21:00 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainVM.cs
@@ -0,0 +1,38 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class MainVM : ViewModelBase
+    {
+        private ICommand _buttonOkCommand;
+
+        public ICommand ButtonOkCommand 
+        {
+            get
+            {
+                return _buttonOkCommand ?? (_buttonOkCommand = new CommandHandler(() => OnClick(), () => CanExecute));
+            }
+        }
+
+        public bool CanExecute
+        {
+            get
+            {
+                // check if executing is allowed, i.e., validate, check if a process is running, etc. 
+                return true;
+            }
+        }
+
+        public void OnClick()
+        {
+
+
+        }
+    }
+}
